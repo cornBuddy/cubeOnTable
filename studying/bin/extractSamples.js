6eb1e3ef5ee72f941f660cd443bf1e6efdbba3ed
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-05 23:07:09 +0300

    image extracting is working

--- studying/bin/extractSamples.js
+++ studying/bin/extractSamples.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 const path = require('path');
 const cv = require('opencv');
+const inspect = require('util').inspect;
 
-const POSITIVE_VIDEO = path.join(__dirname, 'positive.3gp');
-const POSITIVE_OUTPUT = path.join(__dirname, 'positive/');
-const NEGATIVE_VIDEO = path.join(__dirname, 'negative.3gp');
-const NEGATIVE_OUTPUT = path.join(__dirname, 'negative/');
+const POSITIVE_VIDEO = path.join(__dirname, '..', 'positive.3gp');
+const POSITIVE_OUTPUT = path.join(__dirname, '..', 'positive/');
+const NEGATIVE_VIDEO = path.join(__dirname, '..', 'negative.3gp');
+const NEGATIVE_OUTPUT = path.join(__dirname, '..', 'negative/');
 
-function extractImages(pathToVideo, outputDir, offset=5) {
+function extractImages(pathToVideo, outputDir) {
   const video = new cv.VideoCapture(pathToVideo);
   let i = 0;
   const iter = function() {
     video.read((err, mat) => {
-      if (err) throw err;
       i++;
-      if (i % offset === 0) {
-        mat.save(path.join(outputDir, `img_${i}.jpg`));
-        console.log(path.join(outputDir, `img_${i}.jpg`));
-      }
+      if (video.getFrameCount() < i)
+        return;
+      mat.save(path.join(outputDir, `img_${i}.jpg`));
+      console.log(path.join(outputDir, `img_${i}.jpg`));
       iter();
     });
   };
@@ -25,4 +25,4 @@ function extractImages(pathToVideo, outputDir, offset=5) {
 }
 
 extractImages(POSITIVE_VIDEO, POSITIVE_OUTPUT);
-extractImages(NEGATIVE_VIDEO, NEGATIVE_OUTPUT, 3);
+extractImages(NEGATIVE_VIDEO, NEGATIVE_OUTPUT);
