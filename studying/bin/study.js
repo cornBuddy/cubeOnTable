ed15742f13ee40584db933b98a263a12c3cfd0b2
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-05 23:07:42 +0300

    extract constants in study script

--- studying/bin/study.js
+++ studying/bin/study.js
@@ -1,12 +1,18 @@
+#!/usr/bin/env node
 const exec = require('child_process').execSync;
 
 const POSITIVE_SAMPLES_COUNT = 6000;
 const WIDTH = 24;
 const HEIGTH = 24;
 const BUF_SIZE = 1024;
+const NUM_STAGES = 20;
+const MIN_HIT_RATE = 0.999;
 
-exec('rm {positive,negative}/*');
-exec('rm samples/*');
+try {
+  exec('rm {positive,negative,samples}/*');
+} catch (_) {
+  console.log('nothing to remove from ./{positive,negative,samples}/*');
+}
 exec('bin/extractSamples.js');
 exec('find ./negative/ -name "*.jpg" > negative.dat');
 exec('find ./positive/ -name "*.jpg" > positive.dat');
@@ -17,9 +23,13 @@ exec(`perl bin/createsamples.pl positive.dat negative.dat samples`
   + ` 2> samples.err.log 1> samples.log`);
 exec('python tools/mergevec.py -v samples/ -o samples.vec');
 const numNeg = parseInt(exec('ls negative/ | wc -l'));
+const numPos = Math.floor(POSITIVE_SAMPLES_COUNT
+  + (NUM_STAGES - 1) * (1 - MIN_HIT_RATE)
+  * POSITIVE_SAMPLES_COUNT + NUM_NEG);
 exec(`opencv_traincascade -data classifier -vec samples.vec`
-  + ` -bg negative.dat -numStages 20 -minHitRate 0.99 -maxFalseAlarmRate 0.5`
-  + ` -numPos ${POSITIVE_SAMPLES_COUNT} -numNeg ${numNeg}`
+  + ` -bg negative.dat -numStages ${NUM_STAGES} -minHitRate ${MIN_HIT_RATE}`
+  + ` -maxFalseAlarmRate 0.5`
+  + ` -numPos ${numPos} -numNeg ${numNeg}`
   + ` -w ${WIDTH} -h ${HEIGHT} -mode ALL`
   + ` -precalcValBufSize ${BUF_SIZE} -precalcIdxBufSize ${BUF_SIZE}`
   + ` 2> study.err.log 1> study.log`);
