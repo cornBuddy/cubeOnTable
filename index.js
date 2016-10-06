f6ba4e7a34ebfe6ae6b45cc9da76aba1572944af
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-06 12:20:44 +0300

    some debug info added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const cv = require('opencv');
 
-const CASCADE = './node_modules/opencv/data/haarcascade_frontalface_alt.xml';
+const CASCADE = './node_modules/opencv/data/haarcascade_frontalface_alt2.xml';
+const CAM_HEIGHT = 240;
+const CAM_WIDTH = 320;
+const CAM_FPS = 10;
+const CAM_INTERVAL = 1000 / CAM_FPS;
 
 function detectObjectsFromCamera(camera, window) {
   return function() {
@@ -8,20 +12,24 @@ function detectObjectsFromCamera(camera, window) {
       if (err) throw err;
       im.detectObject(CASCADE, {}, (err, objects) => {
         if (err) throw err;
-        console.log(objects[0]);
-        for (const obj of objects)
+        for (let i = 0; i < objects.length; i++) {
+          const obj = objects[i];
+          console.log(`obj[${i}] = ${obj}`);
           im.rectangle([obj.x, obj.y], [obj.width, obj.height]);
+        }
         window.show(im);
-        window.blockingWaitKey(0, 0);
+        window.blockingWaitKey(0, CAM_FPS);
       });
     });
   }
 }
 
 try {
-  const camera = new cv.VideoCapture(0);
   const window = new cv.NamedWindow('Video', 0);
-  setInterval(detectObjectsFromCamera(camera, window), 100);
+  const camera = new cv.VideoCapture(0);
+  camera.setWidth(CAM_WIDTH);
+  camera.setHeight(CAM_HEIGHT);
+  setInterval(detectObjectsFromCamera(camera, window), CAM_INTERVAL);
 } catch (e){
   console.log("Couldn't start camera:", e)
 }
