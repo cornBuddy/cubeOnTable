112dd60d8fe502ee48ef2077c19173700e92616c
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-06 14:28:12 +0300

    uneeded constant removed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: fix segmentation fault error
 const cv = require('opencv');
 
-const CASCADE = cv.FACE_CASCADE;
 const CAM_HEIGHT = 240;
 const CAM_WIDTH = 320;
 const MAGIC_NUMBER = 200;
@@ -10,7 +9,7 @@ function detectObjectsFromCamera(camera, window) {
   return function() {
     camera.read((err, im) => {
       if (err) throw err;
-      im.detectObject(CASCADE, {}, (err, objects) => {
+      im.detectObject(cv.FACE_CASCADE, {}, (err, objects) => {
         if (err) throw err;
         for (let i = 0; i < objects.length; i++) {
           const obj = objects[i];
