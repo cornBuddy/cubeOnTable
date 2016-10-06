a8c185a339112ebadadb5fa9d2323c9035a67408
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-06 11:43:12 +0300

    test object detection added, but it doesn't work

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const cv = require('opencv');
 
-try {
-  const camera = new cv.VideoCapture(0);
-  const window = new cv.NamedWindow('Video', 0)
-  setInterval(() => {
+const CASCADE = './node_modules/opencv/data/haarcascade_frontalface_alt.xml';
+
+function detectObjectsFromCamera(camera, window) {
+  return function() {
     camera.read((err, im) => {
       if (err) throw err;
-      window.show(im);
-      window.blockingWaitKey(0, 20);
+      im.detectObject(CASCADE, {}, (err, objects) => {
+        if (err) throw err;
+        console.log(objects[0]);
+        for (const obj of objects)
+          im.rectangle([obj.x, obj.y], [obj.width, obj.height]);
+        window.show(im);
+        window.blockingWaitKey(0, 0);
+      });
     });
-  }, 20);
+  }
+}
+
+try {
+  const camera = new cv.VideoCapture(0);
+  const window = new cv.NamedWindow('Video', 0);
+  setInterval(detectObjectsFromCamera(camera, window), 100);
 } catch (e){
   console.log("Couldn't start camera:", e)
 }
