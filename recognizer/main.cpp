83053dea676ea8ec0437a63817cd6ada3430a953
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-06 13:11:08 +0300

    cpp camera test

--- recognizer/main.cpp
+++ recognizer/main.cpp
@@ -1,3 +1,20 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+
 int main() {
+    cv::namedWindow("camera", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture cap;
+    cap.open(0);
+    cv::Mat frame;
+    while (true) {
+        cap >> frame;
+        if (!frame.data)
+            break;
+        cv::imshow("camera", frame);
+        if (cv::waitKey(33) >= 0)
+            break;
+    }
     return 0;
 }
