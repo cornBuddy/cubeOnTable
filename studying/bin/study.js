afa6511a9a45f7fdbb1270dff9177190751b61e1
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-12 19:56:16 +0300

    better quality of studying

--- studying/bin/study.js
+++ studying/bin/study.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 const exec = require('child_process').execSync;
 
-const POSITIVE_SAMPLES_COUNT = 7000;
+const POSITIVE_SAMPLES_COUNT = 5000;
 const WIDTH = 48;
 const HEIGTH = 24;
 const BUF_SIZE = 512;
-const NUM_STAGES = 2;
-const MIN_HIT_RATE = 0.95;
+const NUM_STAGES = 10;
+const MIN_HIT_RATE = 0.99;
 const CLASSIFIER_DIR = 'lbp-classifier';
 const FEATURE_TYPE = 'LBP';
 
-//try {
-//  exec('rm samples/*');
-//} catch (_) {
-//  console.log('nothing to remove from ./samples/*');
-//}
-//exec('find ./negative/ -name "*.jpg" > negative.dat');
-//exec('find ./positive/ -name "*.jpg" > positive.dat');
-//exec(`perl bin/createsamples.pl positive.dat negative.dat samples`
-//  + ` ${POSITIVE_SAMPLES_COUNT}`
-//  + ` "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1`
-//  + ` -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w ${WIDTH} -h ${HEIGTH}"`
-//  + ` 2> samples.err.log 1> samples.log`);
-//exec('python tools/mergevec.py -v samples/ -o samples.vec');
+try {
+  exec('rm samples/*');
+} catch (_) {
+  console.log('nothing to remove from ./samples/*');
+}
+exec('find ./negative/ -name "*.jpg" > negative.dat');
+exec('find ./positive/ -name "*.jpg" > positive.dat');
+exec(`perl bin/createsamples.pl positive.dat negative.dat samples`
+  + ` ${POSITIVE_SAMPLES_COUNT}`
+  + ` "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1`
+  + ` -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w ${WIDTH} -h ${HEIGTH}"`
+  + ` 2> samples.err.log 1> samples.log`);
+exec('python tools/mergevec.py -v samples/ -o samples.vec');
 const numNeg = parseInt(exec('ls negative/ | wc -l'));
 const numPos = Math.floor(
   (POSITIVE_SAMPLES_COUNT - numNeg)
