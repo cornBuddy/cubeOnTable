c28136e66d5d6b9a70d1850fdf95575266ad30a5
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-16 19:17:34 +0300

    egdes filtering added

--- imageProcessing.js
+++ imageProcessing.js
@@ -1,5 +1,14 @@
+const CANNY_LOW = 100;
+const CANNY_HIGH = 200;
+const GAUSSIAN_BLUR_SIZE = [5, 5];
+const DILATE_ITERS = 3;
+
 function filter(image) {
-  return null;
+  const copy = image.copy();
+  copy.gaussianBlur(GAUSSIAN_BLUR_SIZE);
+  copy.canny(CANNY_LOW, CANNY_LOW);
+  copy.dilate(DILATE_ITERS);
+  return copy;
 }
 
 module.exports.filter = filter;
