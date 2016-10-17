90b1a9f611f10ee3e88d263b70e95725be16e36a
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-17 13:15:48 +0300

    entry point logic updated

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // TODO: fix segmentation fault error
 const cv = require('opencv');
-const track = require('./imageProcessing').track;
+const track = require('./imageProcessing').drawCube;
 const findTrackedObject = require('./imageProcessing').findTrackedObject;
 
 const CAM_HEIGHT = 240;
 const CAM_WIDTH = 320;
 const MAGIC_NUMBER = 200;
+const ESC = 27;
 
 let object = null;
 let pid = null;
+
+function initCamera() {
+  const camera = new cv.VideoCapture(0);
+  camera.setWidth(CAM_WIDTH);
+  camera.setHeight(CAM_HEIGHT);
+  return camera;
+}
+
+function initWindow() {
+  return new cv.NamedWindow('Tracking', 0);
+}
 
 function findObject(camera) {
   const search =  function() {
@@ -25,7 +37,17 @@ function findObject(camera) {
   pid = setInterval(search, MAGIC_NUMBER);
 }
 
-const camera = new cv.VideoCapture(0);
-camera.setWidth(CAM_WIDTH);
-camera.setHeight(CAM_HEIGHT);
+function showImageFromCamera(window, camera, cb) {
+  camera.read((err, rawImage) => {
+    if (err) throw err;
+    const imageWithCube = drawCube(rawImage, object);
+    window.show(imageWithCube);
+    if (window.blockingWaitKey(0, 50) === ESC)
+      process.exit(0);
+  });
+}
+
+const camera = initCamera();
 findObject(camera);
+const window = initWindow();
+showImageFromCamera(window, camera);
