f040b7ec1ff0490c49ab2123780940386a455454
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-17 20:07:15 +0300

    debug mode on

--- imageProcessing/index.js
+++ imageProcessing/index.js
@@ -1,6 +1,8 @@
 const findTrack = require('./search').findTrack;
+const filter = require('./search').filter;
 
 const drawCube = require('./draw').drawCube;
 
 module.exports.drawCube = drawCube;
 module.exports.findTrack = findTrack;
+module.exports.filter = filter;
