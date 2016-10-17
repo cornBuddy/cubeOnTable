474473e8b210dcf242465ab19a78dd8c5f7726b9
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-17 17:05:26 +0300

    promisification done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // TODO: fix segmentation fault error
-// TODO: promisificate function calls
 const cv = require('opencv');
 const drawCube = require('./imageProcessing').drawCube;
 const findTrack = require('./imageProcessing').findTrack;
@@ -8,9 +7,6 @@ const CAM_HEIGHT = 240;
 const CAM_WIDTH = 320;
 const MAGIC_NUMBER = 200;
 const ESC = 27;
-
-let track = null;
-let pid = null;
 
 function initCamera() {
   const camera = new cv.VideoCapture(0);
@@ -24,31 +20,38 @@ function initWindow() {
 }
 
 function searchForTrack(camera) {
-  const search =  function() {
-    camera.read((err, rawImage) => {
-      console.log('searching...');
-      track = findTrack(rawImage);
-      if (track) {
-        console.log('found!');
-        clearInterval(pid);
-        return;
-      }
-    });
-  };
-  pid = setInterval(search, MAGIC_NUMBER);
-}
-
-function showImageFromCamera(window, camera, cb) {
-  camera.read((err, rawImage) => {
-    if (err) throw err;
-    const imageWithCube = drawCube(rawImage, track);
-    window.show(imageWithCube);
-    if (window.blockingWaitKey(0, MAGIC_NUMBER) === ESC)
-      process.exit(0);
+  return new Promise((resolve, reject) => {
+    let pid = null;
+    const search =  function() {
+      camera.read((err, rawImage) => {
+        if (err)
+          reject(err);
+        console.log('searching...');
+        let track = findTrack(rawImage);
+        if (track) {
+          console.log('found!', track);
+          clearInterval(pid);
+          resolve(track);
+        }
+      });
+    };
+    pid = setInterval(search, MAGIC_NUMBER);
   });
 }
 
+function showImage(window, camera) {
+  return (track) => {
+    camera.read((err, rawImage) => {
+      if (err) throw err;
+      const imageWithCube = drawCube(rawImage, track);
+      window.show(imageWithCube);
+      if (window.blockingWaitKey(0, MAGIC_NUMBER) === ESC)
+        process.exit(0);
+    });
+  };
+}
+
 const camera = initCamera();
-searchForTrack(camera);
 const window = initWindow();
-showImageFromCamera(window, camera);
+searchForTrack(camera)
+  .then(showImage(window, camera));
