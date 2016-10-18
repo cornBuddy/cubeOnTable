934d041c2f0d6d80f1cb1dddbe714218cce0250c
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-18 16:36:07 +0300

    points sort adde

--- imageProcessing/draw.js
+++ imageProcessing/draw.js
@@ -1,12 +1,12 @@
 const getTrackedObject = require('./search').getTrackedObject;
 
-// bgr
 const BLUE = [255, 0, 0];
 const GREEN = [0, 255, 0];
 
 function drawAxis(image, points) {
-  const o = points[3];
-  image.line([o.x, o.y], [points[0].x, points[0].y], BLUE, 10);
+  points = points.sort((p, c) => p.x - c.x);
+  const o = points[0];
+  image.line([o.x, o.y], [points[1].x, points[1].y], BLUE, 10);
   image.line([o.x, o.y], [points[2].x, points[2].y], GREEN, 10);
   return image;
 }
