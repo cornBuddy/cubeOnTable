c89fca894543c3c31b8017b7c5e5164a62e81256
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-19 18:46:45 +0300

    filtering added

--- python/main.py
+++ python/main.py
@@ -4,13 +4,25 @@ import numpy as np
 import cv2
 
 
+CANNY_LOW = 200
+CANNY_HIGH = 200
+GAUSSIAN_BLUR_SIZE = (7, 7)
+
+
+def filt(image):
+    copy = image.copy()
+    edges = cv2.Canny(copy, CANNY_LOW, CANNY_HIGH)
+    blured = cv2.GaussianBlur(edges, GAUSSIAN_BLUR_SIZE, 0)
+    return blured
+
+
 def main():
     path = sys.argv[1]
     raw_image = cv2.imread(path)
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.imshow('image', raw_image)
+    filtered = filt(raw_image)
+    cv2.imshow('filtered', filtered)
     cv2.waitKey(0)
-    cv2.destroyAllWidnows()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
