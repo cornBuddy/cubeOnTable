19531b684d52ba9e6236c83e1f0a63d863f015af
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-19 18:36:17 +0300

    python show image

--- python/main.py
+++ python/main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+import sys
 import numpy as np
 import cv2
 
 
+def main():
+    path = sys.argv[1]
+    raw_image = cv2.imread(path)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.imshow('image', raw_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWidnows()
+
+
 if __name__ == '__main__':
-    print('hello world')
+    main()
