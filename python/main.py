e076da518a4516e9494b703f98710f98a4e2675f
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-20 21:04:33 +0300

    something drown

--- python/main.py
+++ python/main.py
@@ -47,16 +47,15 @@ def search_for_table_corners(raw_image):
     return None
 
 
-def draw(canvas, projection_points):
-    projection_points = np.int32(projection_points).reshape(-1,2)
+def draw(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1,2)
     # draw ground floor in green
-    img = cv2.drawContours(canvas, [projection_points[:4]],-1,(0,255,0),-3)
+    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
     # draw pillars in blue color
-    for i, j in zip(range(4), range(4,8)):
-        img = cv2.line(img, tuple(projection_points[i]),
-                tuple(projection_points[j]),(255),3)
+    for i,j in zip(range(4),range(4,8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
     # draw top layer in red color
-    img = cv2.drawContours(canvas, [projection_points[4:]],-1,(0,0,255),3)
+    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
     return img
 
 
@@ -102,10 +101,9 @@ def draw_cube(raw_image, table_corners):
     distorsions = generate_distorsions()
     _, rvecs, tvecs = cv2.solvePnP(object_points, corners_subpxs,
             camera_matrix, distorsions)
-    print(rvecs, tvecs)
     projection_points, _ = cv2.projectPoints(axis, rvecs, tvecs,
             camera_matrix, distorsions)
-    canvas = draw(raw_image, projection_points)
+    canvas = draw(raw_image, corners_subpxs, projection_points)
     return canvas
 
 
