3bf0c13b2265e4cafe9b6a16040dcae6423f0143
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-20 16:55:38 +0300

    draw cube function implemented

--- python/main.py
+++ python/main.py
@@ -12,10 +12,11 @@ MIN_RECT_AREA = 100
 IS_CLOSED = True
 DELTA = 0.01
 
-# bgr
 BLUE = (255, 0, 0)
 GREEN = (0, 255, 0)
 RED = (0, 0, 255)
+
+FOCAL = 50 # [25; 50] -> [focal; plane]
 
 
 def show(image):
@@ -41,29 +42,83 @@ def search_for_table_corners(raw_image):
         cnt_len = cv2.arcLength(cnt, IS_CLOSED)
         approx = cv2.approxPolyDP(cnt, DELTA * cnt_len, IS_CLOSED)
         if len(approx) == 4:
-            return sorted(approx, key=lambda x: x[0][0], reverse=False)
+            # return sorted(approx, key=lambda x: x[0][0], reverse=False)
+            crns = sorted(approx, key=lambda x: x[0][0], reverse=False)
+            corners = [c[0] for c in crns]
+            return np.asarray(corners)
     return None
 
 
-def draw_axis(raw_image, points):
-    copy = raw_image.copy()
-    o = tuple(points[0][0])
-    x = tuple(points[1][0])
-    y = tuple(points[2][0])
-    print(o, x, y)
-    cv2.line(copy, o, x, BLUE, 5)
-    cv2.line(copy, o, y, RED, 5)
-    return copy
+def draw(canvas, projection_points):
+    projection_points = np.int32(projection_points).reshape(-1,2)
+    # draw ground floor in green
+    img = cv2.drawContours(canvas, [projection_points[:4]],-1,(0,255,0),-3)
+    # draw pillars in blue color
+    for i, j in zip(range(4), range(4,8)):
+        img = cv2.line(img, tuple(projection_points[i]),
+                tuple(projection_points[j]),(255),3)
+    # draw top layer in red color
+    img = cv2.drawContours(canvas, [projection_points[4:]],-1,(0,0,255),3)
+    return img
+
+
+def generate_camera_matrix(image):
+    fx = 0.5 + FOCAL / 50
+    h, w, _ = image.shape
+    mtx = np.zeros((3, 3), np.float32)
+    mtx[0, 0] = mtx[1, 1] = fx * w
+    mtx[0, 2] = 0.5 * (w - 1)
+    mtx[1, 2] = 0.5 * (h - 1)
+    mtx[2, 2] = 1
+    return mtx
+
+
+def generate_distorsions():
+    return np.zeros((1, 4), np.float32)
+
+
+def get_object_points(corners):
+    objp = np.zeros((6*7,3), np.float32)
+    #objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    print(objp.shape)
+    return objp
+
+
+def draw_cube(raw_image, table_corners):
+    canvas = raw_image.copy()
+    print(canvas.shape)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
+            30, 0.001)
+    object_points = get_object_points(table_corners)
+    axis = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],
+            [0, 0, -3],[0, 3, -3],[3, 3, -3],[3, 0, -3]])
+    #corners_sub_pixs = cv2.cornerSubPix(canvas,
+    #        table_corners, (11, 11), (-1, -1), criteria)
+    camera_matrix = generate_camera_matrix(canvas)
+    distorsions = generate_distorsions()
+    rvecs, tvecs, _ = cv2.solvePnPRansac(object_points, table_corners,
+            camera_matrix, distorsions)
+    projection_points, _ = cv2.projectPoints(axis, rvecs, tvecs,
+            camera_matrix, distorsions)
+    canvas = draw(raw_image, projection_points)
+    return canvas
+
+
+def test():
+    img = cv2.imread('studying/positive/chessboard.jpg')
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    print(ret)
 
 
 def main():
     path = sys.argv[1]
     raw_image = cv2.imread(path)
     table_corners = search_for_table_corners(raw_image)
-    axis = draw_axis(raw_image, table_corners)
-    show(axis)
     if table_corners is None:
         raise Exception('there is no table!')
+    result = draw_cube(raw_image, table_corners)
+    show(result)
 
 
 if __name__ == '__main__':
